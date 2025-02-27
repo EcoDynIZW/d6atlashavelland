859f634fb83bb7f508eb6a3a074299021f857924
Wenzler-Meya <wenzler@izw-berlin.local>
2025-02-27 14:15:06 +0100

    added data

---
+++ R/data.R
@@ -0,0 +1,201 @@
+#' @title Running water courses polygons
+#' @description sf object containing running water courses (biotope type 01)
+#' @format A data frame with 161 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"river_01_poly"
+
+#' @title Running water courses lines
+#' @description sf object containing running water courses (biotope type 01)
+#' @format A data frame with 1440 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"river_01_line"
+
+#' @title Standing water courses polygons
+#' @description sf object containing standing water areas (biotope type 02)
+#' @format A data frame with 373 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"standing_water_02_poly"
+
+#' @title Standing water courses lines
+#' @description sf object containing standing water areas (biotope type 02)
+#' @format A data frame with 1 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"standing_water_02_line"
+
+#' @title ruderal vegetation
+#' @description sf object containing areas of ruderal vegetation (biotope type 03)
+#' @format A data frame with 687 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"ruderal_03_poly"
+
+#' @title swamp & wetland
+#' @description sf object containing areas of swamps & wetland (biotope type 04)
+#' @format A data frame with 214 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"peatlands_04_poly"
+
+#' @title grassland
+#' @description sf object containing areas of grassland (biotope type 05)
+#' @format A data frame with 2749 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"grassland_05_poly"
+
+#' @title small bush land
+#' @description sf object containing areas of small bush land (biotope type 06)
+#' @format A data frame with 19 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"shrubs_06_poly"
+
+#' @title hedges
+#' @description sf object containing areas of hedges (biotope type 07)
+#' @format A data frame with 1115 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"hedges_07_poly"
+
+#' @title hedges
+#' @description sf object containing areas of hedges (biotope type 07)
+#' @format A data frame with 2773 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"hedges_07_line"
+
+#' @title forest
+#' @description sf object containing areas of forest (biotope type 08)
+#' @format A data frame with 3381 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"forest_08_poly"
+
+#' @title agriculture
+#' @description sf object containing areas of agriculture (biotope type 09)
+#' @format A data frame with 1045 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"agriculture_09_poly"
+
+#' @title open areas
+#' @description sf object containing open areas (biotope type 10)
+#' @format A data frame with 1678 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"open_areas_10_poly"
+
+#' @title other
+#' @description sf object containing ther areas (biotope type 11)
+#' @format A data frame with 67 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"other_11_poly"
+
+#' @title built up area
+#' @description sf object containing areas of built-up (biotope type 12)
+#' @format A data frame with 338 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"build_areas_12_poly"
+
+#' @title built up area
+#' @description sf object containing areas of built-up (biotope type 12)
+#' @format A data frame with 5624 rows and 3 variables:
+#' \describe{
+#'   \item{\code{biotoptype}}{character biotope type}
+#'   \item{\code{name_class_eng}}{character biotope text (english)}
+#'   \item{\code{geom}}{list geometry column}
+#'}
+#' @details https://metaver.de/trefferanzeige?docuuid=B57B9F35-AFFF-49F2-BA32-618D1A1CD412
+"build_areas_12_line"
+
+#' @title landuse_reclass_table
+#' @description table with landuse categories and a reclass column
+#' @format a dataframe with 7755 rows and 5 columns
+#' \describe{
+#'   \item{\code{biotoptype }}{numeric biotoptype}
+#'   \item{\code{Name_Klasse}}{name of biotoptype in german}
+#'   \item{\code{BIOTYP8}}{8 character biotoptype }
+#'   \item{\code{Name_8st}}{sub categories in german}
+#'   \item{\code{name_class_eng}}{reclass names in english}
+#'}
+#' @details DETAILS
+"landuse_reclass_table"
+
+#' @title station_grosskreutz
+#' @description sf object with base station location
+#' @format a dataframe with 16 rows and 19 columns
+#' \describe{
+#'   \item{\code{name}}{character Station name}
+#'   \item{\code{x}}{numeric x coordinate}
+#'   \item{\code{y}}{numeric y coordinate}
+#'   \item{\code{geometry}}{list geometry column}
+#'}
+#' @details DETAILS
+"station_grosskreutz"
+
